426ecea020f6b35b8e84895fc118c5d8788b302e
Resdan Logan <rezn5447@gmail.com>
2017-03-20 14:30:38 -0700

    update camera functions add camera icons

--- src/containers/GramCreate.js
+++ src/containers/GramCreate.js
@@ -1,50 +1,253 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Camera from 'react-native-camera';
+import CamFrontWhite from '../assets/camera/ic_camera_front_white.png'
+import CamRearWhite from '../assets/camera/ic_camera_rear_white.png'
+import FlashAutoWhite from '../assets/camera/ic_flash_auto_white.png'
+import FlashOffWhite from '../assets/camera/ic_flash_off_white.png'
+import FlashOnWhite from '../assets/camera/ic_flash_on_white.png'
+import CamFrontWhite from '../assets/camera/ic_camera_front_white.png'
 
-class GramCreate extends Component {
+export default class GramCreate extends Component {
+  constructor(props) {
+    super(props);
 
-  takePicture() {
-    this.camera.capture()
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
+    this.camera = null;
+
+    this.state = {
+      camera: {
+        aspect: Camera.constants.Aspect.fill,
+        captureTarget: Camera.constants.CaptureTarget.cameraRoll,
+        type: Camera.constants.Type.back,
+        orientation: Camera.constants.Orientation.auto,
+        flashMode: Camera.constants.FlashMode.auto,
+      },
+      isRecording: false
+    };
+  }
+
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.capture()
+        .then((data) => console.log(data))
+        .catch(err => console.error(err));
+    }
+  }
+
+  startRecording = () => {
+    if (this.camera) {
+      this.camera.capture({mode: Camera.constants.CaptureMode.video})
+          .then((data) => console.log(data))
+          .catch(err => console.error(err));
+      this.setState({
+        isRecording: true
+      });
+    }
+  }
+
+  stopRecording = () => {
+    if (this.camera) {
+      this.camera.stopCapture();
+      this.setState({
+        isRecording: false
+      });
+    }
+  }
+
+  switchType = () => {
+    let newType;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      newType = front;
+    } else if (this.state.camera.type === front) {
+      newType = back;
+    }
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        type: newType,
+      },
+    });
+  }
+
+  typeIcon() {
+    let icon;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      icon = require('../assets/ic_camera_rear_white.png');
+    } else if (this.state.camera.type === front) {
+      icon = require('./assets/ic_camera_front_white.png');
+    }
+
+    return icon;
+  }
+
+  switchFlash = () => {
+    let newFlashMode;
+    const { auto, on, off } = Camera.constants.FlashMode;
+
+    if (this.state.camera.flashMode === auto) {
+      newFlashMode = on;
+    } else if (this.state.camera.flashMode === on) {
+      newFlashMode = off;
+    } else if (this.state.camera.flashMode === off) {
+      newFlashMode = auto;
+    }
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        flashMode: newFlashMode,
+      },
+    });
+  }
+
+  flashIcon() {
+    let icon;
+    const { auto, on, off } = Camera.constants.FlashMode;
+
+    if (this.state.camera.flashMode === auto) {
+      icon = { CamFrontWhite };
+    } else if (this.state.camera.flashMode === on) {
+      icon = { FlashOnWhite };
+    } else if (this.state.camera.flashMode === off) {
+      icon = { FlashOffWhite };
+    }
+
+    return icon;
   }
 
   render() {
-     return (
-       <View style={styles.container}>
-         <Camera
-           ref={(cam) => {
-             this.camera = cam;
-           }}
-           style={styles.preview}
-           aspect={Camera.constants.Aspect.fill}
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          animated
+          hidden
+        />
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={this.state.camera.aspect}
+          captureTarget={this.state.camera.captureTarget}
+          type={this.state.camera.type}
+          flashMode={this.state.camera.flashMode}
+          defaultTouchToFocus
+          mirrorImage={false}
+        />
+        <View style={[styles.overlay, styles.topOverlay]}>
+          <TouchableOpacity
+            style={styles.typeButton}
+            onPress={this.switchType}
           >
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-         </Camera>
-       </View>
-     );
-   }
- }
+            <Image
+              source={this.typeIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.flashButton}
+            onPress={this.switchFlash}
+          >
+            <Image
+              source={this.flashIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+          {
+            !this.state.isRecording
+            &&
+            <TouchableOpacity
+                style={styles.captureButton}
+                onPress={this.takePicture}
+            >
+              <Image
+                  source={require('./assets/ic_photo_camera_36pt.png')}
+              />
+            </TouchableOpacity>
+            ||
+            null
+          }
+          <View style={styles.buttonsSpace} />
+          {
+              !this.state.isRecording
+              &&
+              <TouchableOpacity
+                  style={styles.captureButton}
+                  onPress={this.startRecording}
+              >
+                <Image
+                    source={require('./assets/ic_videocam_36pt.png')}
+                />
+              </TouchableOpacity>
+              ||
+              <TouchableOpacity
+                  style={styles.captureButton}
+                  onPress={this.stopRecording}
+              >
+                <Image
+                    source={require('./assets/ic_stop_36pt.png')}
+                />
+              </TouchableOpacity>
+          }
+        </View>
+      </View>
+    );
+  }
+}
 
- const styles = {
-   container: {
-     flex: 1,
-     flexDirection: 'row',
-   },
-   preview: {
-     flex: 1,
-     justifyContent: 'flex-end',
-     alignItems: 'center'
-   },
-   capture: {
-     flex: 0,
-     backgroundColor: '#fff',
-     borderRadius: 5,
-     color: '#000',
-     padding: 10,
-     margin: 40
-   }
- };
-
-export default GramCreate;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captureButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  typeButton: {
+    padding: 5,
+  },
+  flashButton: {
+    padding: 5,
+  },
+  buttonsSpace: {
+    width: 10,
+  },
+});
